0408bf8a04984e102a9389e341f582c83df34e27
Francisco <francisco.vera.95@gmail.com>
2025-06-16 16:53:22 -0600

    Estructura del proyecto Springboot

---
+++ api/src/test/java/com/bach/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bach.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
