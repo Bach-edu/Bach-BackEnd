d2011deab41fcaaafc1bf025a230517723068f91
Francisco <francisco.vera.95@gmail.com>
2025-06-18 16:37:41 -0600

    entidades creadas

--- api/src/main/java/com/bach/api/domain/model/Mentoria.java
+++ api/src/main/java/com/bach/api/domain/model/Mentoria.java
@@ -1,4 +1,27 @@
 package com.bach.api.domain.model;
 
+import jakarta.persistence.*;
+import org.hibernate.annotations.CurrentTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity(name = "metoria")
+@Table(name = "mentorias")
 public class Mentoria {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "mentor_id", nullable = false)
+    private Usuario mentor;
+
+    @ManyToOne
+    @JoinColumn(name = "desafio_id", nullable = false)
+    private Desafio desafio;
+
+    private String tema;
+
+    @CurrentTimestamp
+    private LocalDateTime fecha;
 }
--- api/src/main/java/com/bach/api/domain/model/Notification.java
+++ api/src/main/java/com/bach/api/domain/model/Notification.java
@@ -1,4 +1,25 @@
 package com.bach.api.domain.model;
 
+import jakarta.persistence.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity(name = "notificacion")
+@Table(name = "notificaciones")
 public class Notification {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "usuario_destino_id", nullable = false)
+    private Usuario usuario;
+
+    private String tipo;
+    private String contenido;
+    private Boolean leido;
+
+    @CreationTimestamp
+    private LocalDateTime fechaEnvio;
 }
